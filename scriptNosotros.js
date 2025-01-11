0fd885dfde942ae56b52ccc9970de66c991a1456
JonyR316 <jlc.rodriguez316@gmail.com>
2025-01-11 11:48:02 -0500

    Nosotros y tecnico 100% 1

--- scriptNosotros.js
+++ scriptNosotros.js
@@ -7,9 +7,9 @@ const bgs = document.querySelectorAll(".bg");
 let index = 0;
 
 nextBtn.addEventListener("click", () => {
-  index = index < 3 ? index + 1 : 3;
+  index = index < 6 ? index + 1 : 6;
 
-  listInfo.style.transform = `translateY(${index * -25}%)`;
+  listInfo.style.transform = `translateY(${index * -14.5}%)`;
   listImg.style.transform = `translateY(${index * -100}%)`;
 
   bgs[index].classList.add("active");
@@ -19,7 +19,7 @@ prevBtn.addEventListener("click", () => {
   indexPrev = index > 0 ? index : 0;
   index = index > 0 ? index - 1 : 0;
 
-  listInfo.style.transform = `translateY(${index * -25}%)`;
+  listInfo.style.transform = `translateY(${index * -14.5}%)`;
   listImg.style.transform = `translateY(${index * -100}%)`;
 
   bgs[indexPrev].classList.remove("active");
